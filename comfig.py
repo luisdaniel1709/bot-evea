7fa8e061b010d4be74bde532d10bcb24bb0654ba
rockstarcuba <102777282+rockstarcuba@users.noreply.github.com>
2022-04-03 15:43:03 -0400

    Update comfig.py

--- comfig.py
+++ comfig.py
@@ -1,10 +1,10 @@
-BOT_TOKEN = ''
+BOT_TOKEN = '5211163116:AAHhhiAiP2_zdByKtHoRBcM6jILTbR9Dajc'
 API_ID = ''
 API_HASH = ''
-ADMIN_ID = []
+ADMIN_ID = [1180073584]
 
-MOODLE_URL = ''
-MOODLE_USER = ''
-MOODLE_PASSWORD = ''
-PARTES_MB = 
+MOODLE_URL = 'https://evea.uh.cu/'
+MOODLE_USER = 'darian.borges@estudiantes.fbio.uh.cu'
+MOODLE_PASSWORD = 'darian1995'
+PARTES_MB = 100
 
