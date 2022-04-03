67b2028cd1cd010a5f4a349f04b580155b329db0
rockstarcuba <102777282+rockstarcuba@users.noreply.github.com>
2022-04-03 16:22:06 -0400

    Update comfig.py

--- comfig.py
+++ comfig.py
@@ -1,6 +1,6 @@
 BOT_TOKEN = '5211163116:AAHhhiAiP2_zdByKtHoRBcM6jILTbR9Dajc'
-API_ID = ''
-API_HASH = ''
+API_ID = '16536294'
+API_HASH = '0c780f6db3124fafbdc3dcde437e6f21'
 ADMIN_ID = [1180073584]
 
 MOODLE_URL = 'https://evea.uh.cu/'
